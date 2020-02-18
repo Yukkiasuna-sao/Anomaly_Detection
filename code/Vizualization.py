468496d9fda8cd33b4f7601e23e5cade50a3e635
SSANGMAN <qkrtkdals93@gmail.com>
2020-02-18 20:44:47 +0900

    Update

--- code/Vizualization.py
+++ code/Vizualization.py
@@ -37,12 +37,13 @@ from pyod.models.abod import ABOD
 from pyod.models.knn import KNN
 from pyod.models.iforest import IForest
 from pyod.models.ocsvm import OCSVM
+from pyod.models.lof import LOF
 from pyod.models.auto_encoder import AutoEncoder
 from pyod.utils.data import generate_data, get_outliers_inliers
 
 def pyod_vis(method, outliers_fraction = 0.1):
     xx, yy = np.meshgrid(np.linspace(-10, 10, 200), np.linspace(-10, 10, 200))
-    X_train, Y_train = generate_data(n_train = 300, train_only = True, n_features = 2, random_state = 42)
+    X_train, Y_train = generate_data(n_train = 300, train_only = True, n_features = 2, random_state = 43)
     
     outlier_fraction = outliers_fraction
     
@@ -205,6 +206,43 @@ def pyod_vis(method, outliers_fraction = 0.1):
         
         plt.show()
         
+    elif method == 'LOF':
+        clf_name = 'Local Outlier Factor'
+        clf = LOF(contamination = outlier_fraction)
+        
+        plt.figure(figsize = (10,10))
+        
+        clf.fit(X_train)
+        score_pred = clf.decision_function(X_train) * -1
+        y_pred = clf.predict(X_train)
+        n_errors = (y_pred != Y_train).sum()
+        print('Num of Errors :', clf_name, n_errors)
+        
+        threshold = stats.scoreatpercentile(score_pred, 100 * outlier_fraction)
+        
+        z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()]) * -1
+        z = z.reshape(xx.shape)
+        
+        subplot = plt.subplot(1, 1, 1)
+        
+        subplot.contourf(xx, yy, z, levels = np.linspace(z.min(), threshold, 10), cmap = plt.cm.Blues_r)
+        
+        a = subplot.contour(xx, yy, z, levels = [threshold], linewidths = 2, colors = 'red')
+        subplot.contourf(xx, yy, z, levels = [threshold, z.max()], colors = 'orange')
+        b = subplot.scatter(X_train[:-n_outliers, 0], X_train[:-n_outliers, 1], c = 'white', s = 20, edgecolor = 'k')
+        c = subplot.scatter(X_train[-n_outliers:, 0], X_train[-n_outliers:, 1], c = 'black', s = 20, edgecolor = 'k')
+        
+        subplot.axis('tight')
+        
+        subplot.legend([a.collections[0], b, c],
+                  ['Learned Decision Function', 'True Inliers', 'True Outliers'])
+        subplot.set_title(clf_name)
+        subplot.set_xlim((-10, 10))
+        subplot.set_ylim((-10, 10))
+        
+        plt.show()
+        
+                
     elif method == 'AutoEncoder':
         X_train, y_train = generate_data(behaviour = 'new', n_features = 100, train_only = True, random_state = 42)
         train_df = pd.DataFrame(X_train)
@@ -232,3 +270,6 @@ def pyod_vis(method, outliers_fraction = 0.1):
         plt.figure(figsize = (10,10))
         sns.scatterplot(x = 0, y = 1, hue = y_train_scores, data = train_df, palette = 'RdBu_r')
         plt.title('Anomaly Score by 100D to 2D PCA')
+        
+        
+pyod_vis(method = 'LOF')
